a6e49f7d93bde9a0f25a1a763bdb635af3f1a777
Isabella Germinario <isabella.germinario@mail.mcgill.ca>
2026-03-24 12:04:47 -0400

    add the theme to plot

--- code.R
+++ code.R
@@ -1,8 +1,10 @@
 ds <- data.frame(a = rnorm(10),
                  b = letters[1:10])
+
 summary(ds)
 
 library(ggplot2)
 
 ggplot(data = ds) +
-  geom_histogram(aes(x = a))
+  geom_histogram(aes(x = a)) +
+  theme_minimal()
